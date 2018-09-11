2b512e90d93c3c479a14c994fb58d1a2ec2751e3
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-09-11 20:57:13 +0200

    Remove injection related code from DataSource

--- Sources/Shared/DataSource/DataSource.swift
+++ Sources/Shared/DataSource/DataSource.swift
@@ -2,14 +2,4 @@ import Foundation
 
 open class DataSource: NSObject, DataSourceDriven {
   public weak var view: Reloadable?
-
-  public override init() {
-    super.init()
-    Injection.add(observer: self, with: #selector(injected(_:)))
-  }
-
-  @objc open func injected(_ notification: Notification) {
-    guard Injection.objectWasInjected(self, in: notification) else { return }
-    view?.reloadData()
-  }
 }
