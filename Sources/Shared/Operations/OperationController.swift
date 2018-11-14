c1b26719b6959d0ef0614357fa9f872f0e62f51a
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-11-14 08:22:50 +0100

    Improve OperationController

--- Sources/Shared/Operations/OperationController.swift
+++ Sources/Shared/Operations/OperationController.swift
@@ -3,12 +3,24 @@ import Foundation
 public class OperationController<T: Operation> {
   public typealias OperationManagerCompletion = (() -> Void)?
   private let _operationQueue: OperationQueue
+  private let _lock = NSLock()
+  private var _isLocked: Bool = false
   private var _operations = [T]()
-
   public var isExecuting: Bool { return _operationQueue.operations.isNotEmpty }
 
   public init(operationQueue: OperationQueue = .init(maxConcurrentOperationCount: 1) ) {
     self._operationQueue = operationQueue
+  }
+
+  public func cancelAllOperations() {
+    guard _operations.isNotEmpty, !_isLocked else { return }
+    _isLocked = true
+    _lock.lock()
+    _operationQueue.isSuspended = true
+    _operationQueue.cancelAllOperations()
+    _operationQueue.isSuspended = false
+    _lock.unlock()
+    _isLocked = false
   }
 
   @discardableResult
@@ -19,6 +31,7 @@ public class OperationController<T: Operation> {
 
   public func execute(_ completion: OperationManagerCompletion = nil) {
     if _operations.isNotEmpty {
+      _operations.forEach { $0.completionBlock = nil }
       _operations.last?.completionBlock = DispatchQueue.main.wrap(completion)
       execute(_operations)
     } else {
